71602577f0e0e892e9dd86ae524a954b5cdc1b11
dcb9 <bob@phpor.me>
2018-04-16 22:36:43 +0800

    update web

--- web/web.js
+++ web/web.js
@@ -4,10 +4,50 @@ var getElementByID = function (id) {
 
 var convertBtn = getElementByID("convert_btn")
 var inputEle = getElementByID("input")
-var outputEle = getElementByID("httpie_output")
+var outputEle = getElementByID("output")
+var warningsEle = getElementByID("warnings_list")
 
+function convert() {
+  warningsEle.parentNode.classList.add("hidden")
+  outputEle.innerHTML = "converting..."
+
+  while (warningsEle.firstChild) {
+    warningsEle.removeChild(warningsEle.firstChild);
+  }
+
+  result = curl2httpie.Do(inputEle.value)
+  outputEle.innerHTML = result.cmd
+  if (result.error !== "") {
+    console.log(result.error)
+    alert(result.error)
+  }
+  var warnings = result.warnings
+  if (warnings.length > 0) {
+    warningsEle.parentNode.classList.remove("hidden")
+    var len = warnings.length;
+    for (var i = 0; i < len; i++) {
+      var li = document.createElement("li")
+      var textNode = document.createTextNode(warnings[i])
+      li.appendChild(textNode)
+      warningsEle.append(li)
+    }
+  }
+
+  outputEle.select()
+}
+
+inputEle.onkeypress = function(evt) {
+  evt = evt || window.event;
+
+  if (evt.keyCode == 13) {
+    evt.preventDefault()
+    convert()
+    return false
+  }
+
+  return true
+}
 convertBtn.addEventListener("click", function(event) {
-  outputEle.innerHTML = ""
-    outputEle.innerHTML = curl2httpie.Do(inputEle.value)
-    event.preventDefault()
+  event.preventDefault()
+  convert()
 })
