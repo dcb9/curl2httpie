6a2377227f03c253ff9a33ec9f0d6177aea9a8c5
yetingsky <yetingskycs@gmail.com>
2018-05-03 01:02:46 +0800

    quote header

--- connector/curl_test.go
+++ connector/curl_test.go
@@ -31,37 +31,37 @@ func TestCurl2Httpie(t *testing.T) {
 			`http --auth "user:password" --form POST 'local.dev'`,
 		}, {
 			[]string{"curl", "-u", "user:password", "-X", "POST", "-H", "Foo: bar", "--data", "foo=bar", "local.dev"},
-			`http --auth "user:password" --form POST 'local.dev' "Foo:bar" "foo=bar"`,
+			`http --auth "user:password" --form POST 'local.dev' 'Foo:bar' 'foo=bar'`,
 		}, {
 			[]string{"curl", "-u", "user:password", "-X", "POST", "-H", "Content-Type: application/json", "--data", "foo=bar", "local.dev"},
-			`http --auth "user:password" --json POST 'local.dev' "foo=bar"`,
+			`http --auth "user:password" --json POST 'local.dev' 'foo=bar'`,
 		}, {
 			[]string{"curl", "-u", "user:password", "--request", "GET", "local.dev", "-H", "accept: application/json", "-H", "Authorization: jwtToken"},
-			`http --auth "user:password" --json GET 'local.dev' "Authorization:jwtToken"`,
+			`http --auth "user:password" --json GET 'local.dev' 'Authorization:jwtToken'`,
 		}, {
 			[]string{"curl", "-u", "user:", "local.dev"},
 			`http --auth "user:" GET 'local.dev'`,
 		}, {
 			[]string{"curl", "local.dev", "-u", "user:", "-d", "foo=Bar"},
-			`http --auth "user:" --form POST 'local.dev' "foo=Bar"`,
+			`http --auth "user:" --form POST 'local.dev' 'foo=Bar'`,
 		}, {
 			[]string{"curl", "local.dev", "--user-agent", "httpie", "-u", "user:", "-d", "foo=Bar"},
-			`http --auth "user:" --form POST 'local.dev' "User-Agent:httpie" "foo=Bar"`,
+			`http --auth "user:" --form POST 'local.dev' 'User-Agent:httpie' 'foo=Bar'`,
 		}, {
 			[]string{"curl", "local.dev", "--referer", "z.cn", "--user-agent", "httpie", "-u", "user:", "-d", "foo=Bar"},
-			`http --auth "user:" --form POST 'local.dev' "Referer:z.cn" "User-Agent:httpie" "foo=Bar"`,
+			`http --auth "user:" --form POST 'local.dev' 'Referer:z.cn' 'User-Agent:httpie' 'foo=Bar'`,
 		}, {
 			[]string{"curl", "local.dev", "-v", "--referer", "z.cn", "--user-agent", "httpie", "-u", "user:", "-d", "foo=Bar"},
-			`http --verbose --auth "user:" --form POST 'local.dev' "Referer:z.cn" "User-Agent:httpie" "foo=Bar"`,
+			`http --verbose --auth "user:" --form POST 'local.dev' 'Referer:z.cn' 'User-Agent:httpie' 'foo=Bar'`,
 		}, {
 			[]string{"curl", "local.dev", "--verbose", "--referer", "z.cn", "--user-agent", "httpie", "-u", "user:", "-d", "foo=Bar"},
-			`http --verbose --auth "user:" --form POST 'local.dev' "Referer:z.cn" "User-Agent:httpie" "foo=Bar"`,
+			`http --verbose --auth "user:" --form POST 'local.dev' 'Referer:z.cn' 'User-Agent:httpie' 'foo=Bar'`,
 		}, {
 			[]string{"curl", "local.dev", "--verbose", "--cookie", "NAME=VAL", "--referer", "z.cn", "--user-agent", "httpie", "-u", "user:", "-d", "foo=Bar"},
-			`http --verbose --auth "user:" --form POST 'local.dev' "Cookie:NAME=VAL" "Referer:z.cn" "User-Agent:httpie" "foo=Bar"`,
+			`http --verbose --auth "user:" --form POST 'local.dev' 'Cookie:NAME=VAL' 'Referer:z.cn' 'User-Agent:httpie' 'foo=Bar'`,
 		}, {
 			[]string{"curl", "-H", "Host: foo.bar.com", "-H", "Accept: */*", "-H", "User-Agent: debug-MyAppName/ CFNetwork/893.14 Darwin/17.4.0", "-H", "Accept-Language: en-us", "--data", "client_id=foobarfoobarfoobar&client_secret=bazquzbazquz&grant_type=password&password=SomePasswordHere&scope=user&username=first.last%2B1%40domain.com", "--compressed", "https://stage.buildsafely.com/api/oauth/token"},
-			`http --form POST 'https://stage.buildsafely.com/api/oauth/token' "Host:foo.bar.com" "Accept:*/*" "User-Agent:debug-MyAppName/ CFNetwork/893.14 Darwin/17.4.0" "Accept-Language:en-us" "client_id=foobarfoobarfoobar&client_secret=bazquzbazquz&grant_type=password&password=SomePasswordHere&scope=user&username=first.last%2B1%40domain.com"`,
+			`http --form POST 'https://stage.buildsafely.com/api/oauth/token' 'Host:foo.bar.com' 'Accept:*/*' 'User-Agent:debug-MyAppName/ CFNetwork/893.14 Darwin/17.4.0' 'Accept-Language:en-us' 'client_id=foobarfoobarfoobar&client_secret=bazquzbazquz&grant_type=password&password=SomePasswordHere&scope=user&username=first.last%2B1%40domain.com'`,
 		},
 	}
 
